f1e90042212f0b3c1e56733491e89ffdf858583a
Robert <rcrooks@brightcove.com>
2023-05-16 15:48:19 -0400

    wip

---
+++ _site/install-submodules.sh
@@ -0,0 +1,36 @@
+git submodule add https://github.com/BrightcoveLearning/s-scripts
+git submodule add https://github.com/BrightcoveLearning/s-doc-updates
+cd _data
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-data
+cd ../_includes
+git submodule add https://github.com/BrightcoveLearning/s-jekyll-page-includes
+git submodule add https://github.com/BrightcoveLearning/s-admin
+git submodule add https://github.com/BrightcoveLearning/s-apis-general
+git submodule add https://github.com/BrightcoveLearning/s-general
+git submodule add https://github.com/BrightcoveLearning/s-media
+git submodule add https://github.com/BrightcoveLearning/s-support
+git submodule add https://github.com/BrightcoveLearning/s-training
+git submodule add https://github.com/BrightcoveLearning/s-misc
+git submodule add https://github.com/BrightcoveLearning/s-brightcove-interactivity
+git submodule add https://github.com/BrightcoveLearning/s-live
+git submodule add https://github.com/BrightcoveLearning/s-template-page-includes
+cd ..
+cd assets/images
+git submodule add https://github.com/BrightcoveLearning/s-admin-img
+git submodule add https://github.com/BrightcoveLearning/s-apis-general-img
+git submodule add https://github.com/BrightcoveLearning/s-general-img
+git submodule add https://github.com/BrightcoveLearning/s-media-img
+git submodule add https://github.com/BrightcoveLearning/s-support-img
+git submodule add https://github.com/BrightcoveLearning/s-training-img
+git submodule add https://github.com/BrightcoveLearning/s-feather-img
+git submodule add https://github.com/BrightcoveLearning/s-misc-img
+git submodule add https://github.com/BrightcoveLearning/s-live-img
+git submodule add https://github.com/BrightcoveLearning/s-brightcove-interactivity-img
+cd ..
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-favicons
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-stylesheets
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-template-stylesheets
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-scripts
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-images
+cd ..
+git submodule update --init --recursive
